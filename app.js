508145fcd850bfd786d9ba6ebdf507184d1fc126
Jason McGinty <jason_mcginty@yahoo.com>
2021-02-20 00:18:42 -0600

    reconfiguring CORS again

--- app.js
+++ app.js
@@ -2,12 +2,16 @@
 
 
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const { authenticateJWT } = require('./middleware/auth');
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: 'https://flexchess.surge.sh' }));
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'https://flexchess.surge.sh');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 app.use(authenticateJWT);
 
 const ExpressError = require('./expressError');
