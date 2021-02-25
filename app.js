2713bc8e8b6c62af9e656107a53ce8ff96855337
Jason McGinty <jason_mcginty@yahoo.com>
2021-02-24 18:14:55 -0600

    reconfiguring CORS again

--- app.js
+++ app.js
@@ -5,8 +5,28 @@ const express = require('express');
 const cors = require('cors');
 const { authenticateJWT } = require('./middleware/auth');
 const app = express();
-
 app.use(cors({ credentials: true }));
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://flexchess.surge.sh');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use(express.json());
 // app.use(function (req, res, next) {
 //     res.header('Access-Control-Allow-Origin', 'https://flexchess.surge.sh');
